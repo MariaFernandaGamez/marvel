a34ce668a02cee70cd5f42b90a88b6267e878854
MariaFernandaGamez <marfgamez04@gmail.com>
2024-05-01 18:13:15 -0500

    primer commit :tada:

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function addInput() {
+    var container = document.getElementById('container');
+    var newInput = document.createElement('input');
+    newInput.type = 'text';
+    container.appendChild(newInput);
+}
