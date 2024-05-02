8bcead773ce126593b6f4adbf62a467d4fe74b86
MariaFernandaGamez <marfgamez04@gmail.com>
2024-05-02 07:34:59 -0500

    avance de imagen :sparkles:

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
-function addInput() {
-    var container = document.getElementById('container');
-    var newInput = document.createElement('input');
-    newInput.type = 'text';
-    container.appendChild(newInput);
+function mostrarImagen() {
+    //Obtiene el elemento <select> con el id "imagenSeleccionada" del documento.
+    let select = document.getElementById('imagenSeleccionada');
+    //Obtiene el valor de la opción seleccionada en ese <select>.
+    let selectedImage = select.options[select.selectedIndex].value;
+    let container = document.getElementById('imagenMostrada');
+    container.innerHTML = '<img src="' + selectedImage + '" alt="Imagen seleccionada">';
 }
+
